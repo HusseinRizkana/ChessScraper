53c9519aba49512f1188dca7bcb54034f35ba36e
HusseinRizkana <Hussein.rizkana@gmail.com>
2021-03-17 13:00:28 +0200

    added function to retrieve player names dictionary  by title

--- ChessComData.py
+++ ChessComData.py
@@ -6,12 +6,24 @@ from time import sleep
 import json
 import os
 
-titles = ["GM","IM"] # Rank to retrieve
-cors = [get_titled_players(title) for title in titles] #coroutines for the case of multiple title request
-response = Client.loop.run_until_complete(gather(*cors))
-grand_master_names =response[0].json["players"]
-internation_master_names = response[1].json["players"]
+# titles = ["GM","IM"] # Rank to retrieve
+# cors = [get_titled_players(title) for title in titles] #coroutines for the case of multiple title request
+# response = Client.loop.run_until_complete(gather(*cors))
+# grand_master_names =response[0].json["players"]
+# internation_master_names = response[1].json["players"]
+def get_names_by_title(titles):
+    ''' 
+    takes in titles of players and returns a dictionary by title of all players holding that title
+    '''
+    names_by_title = {}
+    cors = [get_titled_players(title) for title in titles] #coroutines for the case of multiple title request
+    response = Client.loop.run_until_complete(gather(*cors))
+    for i,name in enumerate(titles):
+        names_by_title[name] = response[i].json["players"]
+    return names_by_title
 
+
+names = get_names_by_title(["GM","IM"])
 nothings = 0
 successes= 0 
 # pprint(grand_master_name)
