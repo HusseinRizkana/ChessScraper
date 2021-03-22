5ecbce8323d4a0a156a2f7ba95fbb9d858b753ac
HusseinRizkana <Hussein.rizkana@gmail.com>
2021-03-22 13:47:26 +0200

    added database interaction library and Main code

---
+++ Test.py
@@ -0,0 +1,116 @@
+# %%
+from ChessScraper.ChessComData import (get_player_data, get_names_by_title,
+                                       retrieve_games_per_month_series, save_player_games, extract_game_pgn)
+from boto3 import resource
+from rds_db_interact import (insert_into_table, get_players, get_moves, insert_multi_into_table,
+                           clear_table, get_max_games, get_games)
+
+from Secrets import (username, access_key_ID, secret_access_ID,
+                     DB_password, DB_username, hostname, DB_name)
+
+s3Client = resource("s3", aws_access_key_id=access_key_ID,
+                    aws_secret_access_key=secret_access_ID)
+
+
+# %%
+# get player usernames that are currently holding this title
+names = get_names_by_title(["GM", "IM"])
+# initialize all player games dict and games per month dict
+player_games = {}
+games_per_month = {}
+# %%
+years_list = [2020]  # chosen years
+# months_list = [i for i in range(1, 13)]  # chosen months
+months_list = [1]
+player_games = {}
+folder = "output_data"
+# tables used
+table_players = "players"
+table_games = "games"
+table_moves = "moves"
+# columns to insert into
+columns_moves = "game_id, move_num, white_move, white_clck, black_move, black_clck"
+columns_games = "game_id, player_black_username, player_white_username, result_black, result_white, game_mode, time_control, inc, date, opening, white_elo, black_elo, start_time"
+columns_players = "player_id, username, name, join_date, country_code, streamer_status, title"
+# retrieve players in chosen titles
+for player in names["GM"]:
+    # return dictionary of player data
+    pd = get_player_data(player)
+    
+    # values to be inputted in rds players sql table
+    values_players = f"{pd['player_id']},'{player}','{pd['name']}',{pd['join_date']}," + \
+        f"'{pd['country_code']}','{pd['is_streamer']}','{pd['title']}'"
+    # saving to rds table players
+    insert_into_table(table_players,columns_players,values_players)
+    player_games = retrieve_games_per_month_series(
+        player, months_list, years_list)
+    #saving to disk
+    save_player_games('output_data',player_games,player,s3Client,disk_or_cloud="disk")
+    #saving to cloud s3
+    save_player_games('output_data',player_games,player,s3Client,disk_or_cloud="cloud",bucket='chess-games-js')
+    #iterate over years and months chosen
+    for year in years_list:
+        for month in months_list:
+            #iterate over retrieved games in that month
+            for game in player_games[year][month]:
+                #extract game portable game notation data
+                pgn = game["pgn"]
+
+                game_num = get_max_games() + 1
+                # translate game into usable structure for SQL insert
+                moves_data, game_date, game_time, game_opening = extract_game_pgn(
+                    pgn)
+                start_time = game_time
+                player_black_username= game["black"]["username"]
+                player_white_username= game["white"]["username"]
+                result_black= game["black"]["result"]
+                result_white= game["white"]["result"]
+                game_mode= game["rules"]
+                timeclass = game["time_control"].split("+")
+                # accounting for cases where there is no increment
+                time_control = ["NULL","NULL"]
+                for i in range(len(timeclass)):
+                    time_control[i] = timeclass[i]
+                date= game_date
+                opening= game_opening
+                white_elo=game["white"]["rating"]
+                black_elo=game["black"]["rating"]
+                
+                game_id = str(game_num)
+                values_games = "{},'{}','{}','{}','{}','{}',{},{},'{}','{}',{},{},'{}'".format(game_id,
+                                player_black_username, player_white_username, result_black, result_white,
+                                game_mode, time_control[0], time_control[1], date, opening, white_elo, black_elo, start_time)
+                MD = [[game_id]+moves_data[i] for i in range(len(moves_data))]
+                #map into a list of tuples for single multiple insert
+                MD = list(map(tuple, MD))
+                
+                # print(MD)
+                values_moves = ','.join("({}, {}, '{}', {}, '{}', {})".format(
+                    i, j, k, x, y, z) for i, j, k, x, y, z in MD)
+                # #insert into moves table
+                insert_into_table("games",columns_games,values_games)
+                insert_multi_into_table("moves",columns_moves,values_moves)
+               
+
+
+               
+
+
+get_players()
+get_games()
+get_moves()
+# %%
+
+
+# select output destination folder
+
+# Iterates through all the objects, doing the pagination for you. Each obj
+# is an ObjectSummary, so it doesn't contain the body. You'll need to call
+# get to get the whole body.
+# %%
+# bucket = s3Client.Bucket('chess-games-js')
+# for obj in bucket.objects.all():
+#     key = obj.key
+#     body = obj.get()['Body'].read()
+# print(body)
+# %%
---
+++ rds_db_interact.py
@@ -0,0 +1,306 @@
+# %%
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2 import sql
+from Secrets import (username, access_key_ID, secret_access_ID,
+                     DB_password, DB_username, hostname, DB_name)
+
+# %%
+if __name__ == "__main__":
+    cnn = psycopg2.connect(host=hostname,
+                       dbname=DB_username,
+                       user=DB_username,
+                       password=DB_password)
+    cnn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    curs = cnn.cursor()
+# %%
+# retrieve all countries table data
+
+
+def get_moves():
+    """ query data from the countries table """
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM moves")
+        print("The number of parts: ", cur.rowcount)
+        # row = cur.fetchone()
+
+        # while row is not None:
+        #     print(row)
+        #     row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def get_countries():
+    """ query data from the countries table """
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM countries")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == "__main__":
+    get_countries()
+    get_moves()
+# %%
+# retrieve all players table data
+
+
+def get_players():
+    """ query data from the games table """
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM players")
+        print("The number of parts: ", cur.rowcount)
+        # row = cur.fetchone()
+
+        # while row is not None:
+        #     # print(row)
+        #     row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == "__main__":
+    get_players()
+# %%
+# retrieve all games table data
+
+
+def get_games():
+    """ query data from the games table """
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM games")
+        print("The number of parts: ", cur.rowcount)
+        # row = cur.fetchone()
+
+        # while row is not None:
+        #     print(row)
+        #     row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == "__main__":
+    get_games()
+
+# execute sql string (for creating table if it doesnt exist) and print column names
+
+
+def create_table(sql, tablename):
+    """ query data from the vendors table """
+    cnn = None
+    try:
+        cnn = psycopg2.connect(host=hostname,
+                           dbname=DB_username,
+                           user=DB_username,
+                           password=DB_password)
+
+        cnn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        Curs = cnn.cursor()
+        #select current version of db
+        Curs.execute('''select version()''')
+        Curs.execute(sql)
+        Curs.execute(f"Select * FROM {tablename} LIMIT 0")
+        colnames = [desc[0] for desc in Curs.description]
+        coltypes = [desc[1] for desc in Curs.description]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if cnn is not None:
+            cnn.close()
+    print(colnames)
+    print(coltypes)
+    Curs.close()
+
+
+# %%
+# sql string to create games table
+crt_games = '''CREATE TABLE if not exists games(
+    game_id int, player_black_username varchar(60),player_white_username varchar(60),
+    result_black varchar(30), result_white varchar(30),game_mode varchar(30),time_control int, inc int,
+    date varchar(25), opening varchar(90), PRIMARY KEY(game_id)
+
+)'''
+if __name__ == "__main__":
+    create_table(crt_games, 'games')
+# %%
+# sql string to create players table
+crt_players = '''CREATE TABLE if not exists players(
+    player_id int, username varchar(60), name varchar(80),join_date int,
+   country_code varchar(3), streamer_status varchar(5),title varchar(3), PRIMARY KEY(player_id)
+)'''
+if __name__ == "__main__":
+    create_table(crt_players, 'players')
+# %%
+# sql string to create moves table
+crt_moves = '''CREATE TABLE if not exists moves(
+    game_id int, move int, white_move varchar(4), white_clck int, black_move varchar(4),
+    black_clck int
+)'''
+if __name__ == "__main__":
+    create_table(crt_moves, 'moves')
+# %%
+
+
+def insert_into_table(table, columns, values):
+
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        # select current version of the db
+        cur.execute("SELECT version()")
+        sql_str = sql.SQL(f"INSERT INTO {table}({columns}) VALUES({values})")
+        cur.execute(sql_str)
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def insert_multi_into_table(table, columns, values):
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+
+        # select current version of the db
+        cur.execute("SELECT version()")
+
+        sql_str = sql.SQL(f"INSERT INTO {table}({columns}) VALUES{values}")
+        # print(sql_str)
+        cur.execute(sql_str)
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+def clear_table(table):
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        # select current version of the db
+        cur.execute("SELECT version()")
+        
+        sql_str = sql.SQL(f"Delete FROM {table}")
+        print(sql_str)
+        cur.execute(sql_str)
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+def get_max_games():
+    """ query data from the games table """
+    conn = None
+    try:
+        conn = psycopg2.connect(host=hostname,
+                                dbname=DB_username,
+                                user=DB_username,
+                                password=DB_password)
+        cur = conn.cursor()
+        cur.execute("SELECT max(game_id) FROM games")
+        row = cur.fetchall()
+        max_id = row[0][0]
+            
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        if max_id == None:
+            max_id = 0
+    return max_id
+if __name__ == "__main__":
+    print(get_max_games())
+
+# %%
