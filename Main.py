c0f74e890d280429a06ee723157d27af2e3b2118
HusseinRizkana <Hussein.rizkana@gmail.com>
2021-03-22 13:51:46 +0200

    Module name error resolved

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 # %%
-from ChessScraper.ChessComData import (get_player_data, get_names_by_title,
+from ChessComData import (get_player_data, get_names_by_title,
                                        retrieve_games_per_month_series, save_player_games, extract_game_pgn)
 from boto3 import resource
 from rds_db_interact import (insert_into_table, get_players, get_moves, insert_multi_into_table,
-                           clear_table, get_max_games, get_games)
+                             clear_table, get_max_games, get_games)
 
 from Secrets import (username, access_key_ID, secret_access_ID,
                      DB_password, DB_username, hostname, DB_name)
@@ -36,24 +36,26 @@ columns_players = "player_id, username, name, join_date, country_code, streamer_
 for player in names["GM"]:
     # return dictionary of player data
     pd = get_player_data(player)
-    
+
     # values to be inputted in rds players sql table
     values_players = f"{pd['player_id']},'{player}','{pd['name']}',{pd['join_date']}," + \
         f"'{pd['country_code']}','{pd['is_streamer']}','{pd['title']}'"
     # saving to rds table players
-    insert_into_table(table_players,columns_players,values_players)
+    insert_into_table(table_players, columns_players, values_players)
     player_games = retrieve_games_per_month_series(
         player, months_list, years_list)
-    #saving to disk
-    save_player_games('output_data',player_games,player,s3Client,disk_or_cloud="disk")
-    #saving to cloud s3
-    save_player_games('output_data',player_games,player,s3Client,disk_or_cloud="cloud",bucket='chess-games-js')
-    #iterate over years and months chosen
+    # saving to disk
+    save_player_games('output_data', player_games, player,
+                      s3Client, disk_or_cloud="disk")
+    # saving to cloud s3
+    save_player_games('output_data', player_games, player,
+                      s3Client, disk_or_cloud="cloud", bucket='chess-games-js')
+    # iterate over years and months chosen
     for year in years_list:
         for month in months_list:
-            #iterate over retrieved games in that month
+            # iterate over retrieved games in that month
             for game in player_games[year][month]:
-                #extract game portable game notation data
+                # extract game portable game notation data
                 pgn = game["pgn"]
 
                 game_num = get_max_games() + 1
@@ -61,39 +63,35 @@ for player in names["GM"]:
                 moves_data, game_date, game_time, game_opening = extract_game_pgn(
                     pgn)
                 start_time = game_time
-                player_black_username= game["black"]["username"]
-                player_white_username= game["white"]["username"]
-                result_black= game["black"]["result"]
-                result_white= game["white"]["result"]
-                game_mode= game["rules"]
+                player_black_username = game["black"]["username"]
+                player_white_username = game["white"]["username"]
+                result_black = game["black"]["result"]
+                result_white = game["white"]["result"]
+                game_mode = game["rules"]
                 timeclass = game["time_control"].split("+")
                 # accounting for cases where there is no increment
-                time_control = ["NULL","NULL"]
+                time_control = ["NULL", "NULL"]
                 for i in range(len(timeclass)):
                     time_control[i] = timeclass[i]
-                date= game_date
-                opening= game_opening
-                white_elo=game["white"]["rating"]
-                black_elo=game["black"]["rating"]
-                
+                date = game_date
+                opening = game_opening
+                white_elo = game["white"]["rating"]
+                black_elo = game["black"]["rating"]
+
                 game_id = str(game_num)
                 values_games = "{},'{}','{}','{}','{}','{}',{},{},'{}','{}',{},{},'{}'".format(game_id,
-                                player_black_username, player_white_username, result_black, result_white,
-                                game_mode, time_control[0], time_control[1], date, opening, white_elo, black_elo, start_time)
+                                                                                               player_black_username, player_white_username, result_black, result_white,
+                                                                                               game_mode, time_control[0], time_control[1], date, opening, white_elo, black_elo, start_time)
                 MD = [[game_id]+moves_data[i] for i in range(len(moves_data))]
-                #map into a list of tuples for single multiple insert
+                # map into a list of tuples for single multiple insert
                 MD = list(map(tuple, MD))
-                
+
                 # print(MD)
                 values_moves = ','.join("({}, {}, '{}', {}, '{}', {})".format(
                     i, j, k, x, y, z) for i, j, k, x, y, z in MD)
                 # #insert into moves table
-                insert_into_table("games",columns_games,values_games)
-                insert_multi_into_table("moves",columns_moves,values_moves)
-               
-
-
-               
+                insert_into_table("games", columns_games, values_games)
+                insert_multi_into_table("moves", columns_moves, values_moves)
 
 
 get_players()
